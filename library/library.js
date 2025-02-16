0f6d05e6f669976b3976df1d058203a6834b1d22
SerAbinU1 <abinyt1234@gmail.com>
2025-02-16 16:40:00 +0000

    Add basic library functionality logic and bare html and css.

---
+++ library/library.js
@@ -0,0 +1,49 @@
+let myLibrary = [];
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    displayBooks(myLibrary);
+}
+
+function displayBooks(myLibrary) {
+    myLibrary.forEach(book => {
+        console.log(book);
+    });
+}
+
+//form to add new book
+const sidebar = document.getElementById("sidebar");
+const openBtn = document.getElementById("openSidebar");
+const closeBtn = document.getElementById("closeSidebar");
+
+openBtn.addEventListener("click", () => {
+    sidebar.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.add("hidden");
+});
+
+//handle form submission
+const form = document.getElementById("bookForm");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault;
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(title, author, pages, read);
+
+    form.reset();
+});
